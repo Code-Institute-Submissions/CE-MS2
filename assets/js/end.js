a53a68d0d09cb865d67c30818cce8f4fa1910dcd
Leo Morgan <laumorgan.14@gmail.com>
2020-11-15 15:28:13 +0000

    another commit

--- assets/js/end.js
+++ assets/js/end.js
@@ -6,11 +6,9 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-const MAX_HIGH_SCORES = 500;
+const MAX_HIGH_SCORES = 5; 
 
 finalScore.innerText = mostRecentScore;
-
-
 
 username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
@@ -29,9 +27,9 @@ saveHighScore = e => {
     };
     highScores.push(score);
     highScores.sort( (a,b) => b.score - a.score)
-    highScores.splice(5);
+    highScores.splice(5); //Only Allows top 5 highscores to be displayed
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('/CE-MS2/index.html');
 
 };
